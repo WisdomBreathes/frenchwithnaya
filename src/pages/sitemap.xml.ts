a172fcf60d51526d8cdf6db9018953e5c08c224d
Shehzeb Iftakhar <ishehzeb@gmail.com>
2026-05-18 21:23:46 -0400

    fix: flag dark mode bg, tighter animation timing, SEO og image + twitter cards + sitemap lastmod

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,5 +1,6 @@
 export async function GET() {
   const siteUrl = 'https://wisdombreathes.github.io/frenchwithnaya'
+  const lastmod = new Date().toISOString().split('T')[0]
 
   const pages = [
     { url: '/', priority: '1.0', changefreq: 'monthly' },
@@ -11,6 +12,7 @@ export async function GET() {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages.map(p => `  <url>
     <loc>${siteUrl}${p.url}</loc>
+    <lastmod>${lastmod}</lastmod>
     <changefreq>${p.changefreq}</changefreq>
     <priority>${p.priority}</priority>
   </url>`).join('\n')}
